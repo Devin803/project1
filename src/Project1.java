9137017aae20be44b7fcec32c79e9f084dbf7fe3
devin803 <Dinocencio@student.bridgew.edu>
2021-09-21 21:08:19 -0400

    Finished project1

--- src/Project1.java
+++ src/Project1.java
@@ -1,36 +1,68 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Project1 {
-    public static void main(String [] args) throws IOException {
-        var fileName = "products.txt";
-        var product = Paths.get(fileName);
-        var productName =  new ArrayList<String>();
-        var productPrice = new ArrayList<Float>();
-        var productAmount = new ArrayList<Integer>();
-        var productLines = Files.readAllLines(product);
-        var userResponse = "";
-        var reader = new Scanner(System.in);
-        for (var line : productLines){
-            var splitLine = line.split(",");
-            productName.add(splitLine[0]);
-            var productP = Float.parseFloat(splitLine[1]);
-            productPrice.add(productP);
-            var productA = Integer.parseInt(splitLine[2]);
-            productAmount.add(productA);
-        }
-        while (!userResponse.equals("done")){
-            System.out.println("What do you want to search for?");
-            if(userResponse.equals("products")){
-                System.out.println("There are" + productName + " The price is: " + productPrice + " The amount is:" + productAmount );
+    public static void main(String [] args)
+    {
+        ArrayList<String> productName = new ArrayList<String>();
+        ArrayList<Float> productPrice = new ArrayList<Float>();
+        ArrayList<Integer> productCount = new ArrayList<Integer>();
+
+        try
+        {
+            File file = new File("products.txt");
+            Scanner reader = new Scanner(file);
+            String[] arr;
+
+            while(reader.hasNextLine())
+            {
+                arr = reader.nextLine().split(" ");
+                productName.add(arr[0]);
+                productPrice.add(Float.parseFloat(arr[1]));
+                productCount.add(Integer.parseInt(arr[2]));
             }
-            userResponse = reader.nextLine();
-            if (!userResponse.equals("products")){
-                System.out.println("Sorry, there arent any of that product");
+            reader.close();
+
+        Scanner input = new Scanner(System.in);
+        int choice;
+        String prod;
+        boolean found;
+        while(true)
+        {
+            found = false;
+            System.out.println("Do you wish to exit? Press 0, else press 1: ");
+            choice = input.nextInt();
+            if (choice == 0)
+            {
+                System.exit(0);
+            }
+            else
+            {
+                System.out.println("Which product are you looking for?");
+                prod = input.next();
+
+                for(int i=0; i<productName.size(); ++i)
+                {
+                    if(productName.get(i).equalsIgnoreCase(prod))
+                    {
+                        System.out.println("The count of the product: " + prod + " is: " + productCount.get(i));
+                        System.out.println("The amount of the product: " + prod + " is: " + productPrice.get(i));
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(found == false)
+                {
+                    System.out.println("Sorry, we don't have that product");
+                }
             }
         }
     }
+        catch (Exception e)
+        {
+        }
+
+        }
 }
